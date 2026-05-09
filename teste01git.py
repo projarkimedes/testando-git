10b34c25762ab9898e0b3ee7152190b15fbbb85a
Edinho <gobboam@gmail.com>
2026-05-09 12:09:05 -0300

    Agora estou na secundaria

--- teste01git.py
+++ teste01git.py
@@ -1,3 +1,4 @@
 print("Testando git no vscode com python no Ubuntu")
 print("Testando mais uma linha de código")
 print("Testando mais uma linha de código para o commit")
+print("Testando mais uma linha de código para secundaria")
